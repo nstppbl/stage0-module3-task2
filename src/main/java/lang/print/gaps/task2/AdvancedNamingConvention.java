a088d945a74e98286c9d7cbe23e92f0d00ce7873
nstppbl <1nstppbl@gmail.com>
2022-08-23 12:55:34 +0500

    fix some issues

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,10 +1,10 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
-    private static final int ADULT_AGE = 18;
+    public static final int ADULT_AGE = 18;
 
-    private int age;
-    private int phoneNumber;
+    int age;
+    int phoneNumber;
 
     void callToFriend() {
         //some code
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable2 = 2000;
+        int variableTwo = 2000;
         int value = 1000;
-        int variable3 = 4000;
-        int value2 = 3000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
         int variables = 5000;
-        int variables2 = 7000;
-        int value3 = 90000;
+        int variablesTwo = 7000;
+        int valueThree = 90000;
     }
 }
